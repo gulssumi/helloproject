b975b5941f3d57bb5114f8280518a3877335d510
Administrator <Administrator@DESKTOP-BOBPMQQ>
2017-12-12 20:24:33 +0900

    second submit

---
+++ Helloproject/Helloproject/Controllers/AbcController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Helloproject.Controllers
+{
+    public class AbcController : Controller
+    {
+        // GET: Abc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
